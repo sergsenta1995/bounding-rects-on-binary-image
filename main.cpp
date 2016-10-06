e13f2cb3c1eb990973926ac25366d3d0b2a492f6
sergsenta1995 <sergsenta1995@yandex.ru>
2016-10-06 17:03:05 +0300

    read/show image

--- main.cpp
+++ main.cpp
@@ -1,4 +1,21 @@
+//
+// Created by sergsenta1995 on 06.10.16.
+//
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
 int main(int argc, char** argv)
 {
+	cv::Mat image;
+	image = cv::imread("/home/sergsenta1995/AIFIL/1.jpg");
+	std::string name_image = "Binary image";
+	cv::namedWindow(name_image);
+
+	cv::imshow(name_image, image);
+
+	cv::waitKey(0);
+
 	return 0;
 }
