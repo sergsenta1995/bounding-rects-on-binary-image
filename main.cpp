94621355927daa59984b529b03899f3a17a2cbfa
sergsenta1995 <sergsenta1995@yandex.ru>
2016-10-07 18:02:45 +0300

    create draw_bounding_rects()

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgcodecs.hpp>
-#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 #include <iostream>
 
@@ -17,14 +17,26 @@ std::vector<cv::Rect> copmute_bounding_rects(const std::vector<std::vector<cv::P
 {
 	std::vector<cv::Rect> result;
 
-	for (auto i = 0; i < contours.size(); i++)
+	for (auto contour : contours)
 	{
-		auto contour = contours[i];
 		cv::Rect bounding_rect = cv::boundingRect(contour);
 		result.push_back(bounding_rect);
 	}
 
 	return result;
+}
+
+void draw_bounding_rects(const std::vector<cv::Rect> &bounding_rects, const cv::Mat &image)
+{
+	std::string name_bounding_rects_image = "Bounding rect";
+	cv::namedWindow(name_bounding_rects_image);
+	cv::Mat result_image = image.clone();
+	for (auto rect : bounding_rects) {
+		cv::rectangle(result_image, rect, cv::Scalar(50));
+		std::cout << "x: " << rect.x <<"; y: "<< rect.y << std::endl;
+	}
+
+	cv::imshow(name_bounding_rects_image, result_image);
 }
 
 int main(int argc, char** argv)
@@ -37,11 +49,14 @@ int main(int argc, char** argv)
 
 
 	std::vector<std::vector<cv::Point>> contours;
+	cv::Mat invert_image = image.clone();
+	cv::threshold(invert_image, invert_image, 70, 255, CV_THRESH_BINARY_INV);
 	cv::findContours(
-			image.clone(),
+			invert_image.clone(),
 			contours,
-			CV_RETR_LIST,
+			CV_RETR_EXTERNAL,
 			CV_CHAIN_APPROX_SIMPLE);
+
 
 	if (contours.size() == 0)
 	{
@@ -49,6 +64,20 @@ int main(int argc, char** argv)
 		cv::waitKey(0);
 		return 0;
 	}
+
+/*
+	cv::Mat drawing = cv::Mat::zeros( image.size(), CV_8UC3 );
+	for( int i = 0; i< contours.size(); i++ )
+	{
+
+		drawContours( drawing, contours, i, cv::Scalar(255));//, 2, 8, hierarchy, 0, Point() );
+	}
+
+	cv::namedWindow( "Contours");
+	cv::imshow( "Contours", drawing );
+*/
+	std::vector<cv::Rect> bounding_rects = copmute_bounding_rects(contours);
+	draw_bounding_rects(bounding_rects, image);
 
 	cv::waitKey(0);
 
