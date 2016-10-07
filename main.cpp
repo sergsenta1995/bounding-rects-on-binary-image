389203bc29ff8d935c9e8afbabed5ae543bebc36
sergsenta1995 <sergsenta1995@yandex.ru>
2016-10-07 18:07:23 +0300

    correction  copmute_bounding_rects()

--- main.cpp
+++ main.cpp
@@ -13,9 +13,23 @@
  * @param contours Вектор векторов точек, т. е. собственно контур.
  * @return Вектор ограничивающих прямоугольников.
  */
-std::vector<cv::Rect> copmute_bounding_rects(const std::vector<std::vector<cv::Point>> &contours)
+std::vector<cv::Rect> copmute_bounding_rects(const cv::Mat &image)
 {
+	std::vector<std::vector<cv::Point>> contours;
+	cv::Mat invert_image;
+	cv::threshold(image, invert_image, 70, 255, CV_THRESH_BINARY_INV);
+	cv::findContours(
+			invert_image,
+			contours,
+			CV_RETR_EXTERNAL,
+			CV_CHAIN_APPROX_SIMPLE);
+
 	std::vector<cv::Rect> result;
+	if (contours.size() == 0)
+	{
+		std::cout << "Contours not found." << std::endl;
+		return result;
+	}
 
 	for (auto contour : contours)
 	{
@@ -47,36 +61,7 @@ int main(int argc, char** argv)
 	cv::namedWindow(name_image);
 	cv::imshow(name_image, image);
 
-
-	std::vector<std::vector<cv::Point>> contours;
-	cv::Mat invert_image = image.clone();
-	cv::threshold(invert_image, invert_image, 70, 255, CV_THRESH_BINARY_INV);
-	cv::findContours(
-			invert_image.clone(),
-			contours,
-			CV_RETR_EXTERNAL,
-			CV_CHAIN_APPROX_SIMPLE);
-
-
-	if (contours.size() == 0)
-	{
-		std::cout << "Contours not found." << std::endl;
-		cv::waitKey(0);
-		return 0;
-	}
-
-/*
-	cv::Mat drawing = cv::Mat::zeros( image.size(), CV_8UC3 );
-	for( int i = 0; i< contours.size(); i++ )
-	{
-
-		drawContours( drawing, contours, i, cv::Scalar(255));//, 2, 8, hierarchy, 0, Point() );
-	}
-
-	cv::namedWindow( "Contours");
-	cv::imshow( "Contours", drawing );
-*/
-	std::vector<cv::Rect> bounding_rects = copmute_bounding_rects(contours);
+	std::vector<cv::Rect> bounding_rects = copmute_bounding_rects(image);
 	draw_bounding_rects(bounding_rects, image);
 
 	cv::waitKey(0);
