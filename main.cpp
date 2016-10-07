e31b578af5ba4104c891d6e1887afa1a14b6b2ae
sergsenta1995 <sergsenta1995@yandex.ru>
2016-10-07 18:19:53 +0300

    add error processing, refactoring

--- main.cpp
+++ main.cpp
@@ -8,9 +8,10 @@
 #include <opencv2/imgproc.hpp>
 #include <iostream>
 
+
 /**
  * @brief Вычисляет ограничивающие прямоугольники из контуров.
- * @param contours Вектор векторов точек, т. е. собственно контур.
+ * @param image Входное изображение.
  * @return Вектор ограничивающих прямоугольников.
  */
 std::vector<cv::Rect> copmute_bounding_rects(const cv::Mat &image)
@@ -40,23 +41,46 @@ std::vector<cv::Rect> copmute_bounding_rects(const cv::Mat &image)
 	return result;
 }
 
+
+/**
+ * @brief Рисует прямоугольники на изображении.
+ * @param bounding_rects Вектор (ограничивающих) прямоугольников.
+ * @param image Входное изображение, на котором будем рисовать.
+ */
 void draw_bounding_rects(const std::vector<cv::Rect> &bounding_rects, const cv::Mat &image)
 {
 	std::string name_bounding_rects_image = "Bounding rect";
 	cv::namedWindow(name_bounding_rects_image);
 	cv::Mat result_image = image.clone();
-	for (auto rect : bounding_rects) {
+	for (auto rect : bounding_rects)
 		cv::rectangle(result_image, rect, cv::Scalar(50));
-		std::cout << "x: " << rect.x <<"; y: "<< rect.y << std::endl;
-	}
 
 	cv::imshow(name_bounding_rects_image, result_image);
 }
 
+
+/**
+ * @brief Находит на чёрно-блом изображении символы, и возвращает их ограничивающие прямоугольники.
+ * @param argc Количество аргументов командной строки - 2 (первый по умолчанию).
+ * @param argv Аргументы командной строки: необходимо передать путь к изображению.
+ */
 int main(int argc, char** argv)
 {
+	if (argc < 2)
+	{
+		std::cout << "Error. Command line argument < 2." << std::endl;
+		std::cout << "Program aborted." << std::endl;
+		return 0;
+	}
 	cv::Mat image;
-	image = cv::imread("/home/sergsenta1995/AIFIL/1.jpg", CV_LOAD_IMAGE_GRAYSCALE);
+	image = cv::imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
+
+	if (image.empty() == true)
+	{
+		std::cout << "Error. Image not found." << std::endl;
+		std::cout << "Program aborted." << std::endl;
+		return 0;
+	}
 	std::string name_image = "Binary image";
 	cv::namedWindow(name_image);
 	cv::imshow(name_image, image);
